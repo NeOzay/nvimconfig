01dda4c6073ce18e4b1dabe305444753d6cbae7a
NeOzay <colpaert.benoit@gmail.com>
2023-03-28 16:39:06 +0000

    phone

---
+++ lua/ozay/neo-tree.lua
@@ -0,0 +1,20 @@
+require("neo-tree").setup({
+    filesystem = {
+        window = {
+      popup = { -- settings that apply to float position only
+        size = {
+          height = "80%",
+          width = "80%",
+        },
+        position = "50%", -- 50% means center it
+            },
+        }
+    }
+})
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<space>fb",
+  ":NeoTreeFloatToggle<CR>",
+  { noremap = true }
+)
